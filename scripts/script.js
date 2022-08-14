bbb471abd1ad667a48eca41db34661e54e3a3dff
dannydelimero <dendelime@gmail.com>
2022-08-14 18:57:25 +0300

    new

--- scripts/script.js
+++ scripts/script.js
@@ -11,4 +11,7 @@ $(document).ready(function() {
         waitForAnimate:false,
         infinite:true,
     });
+    $('.head__burger').click(function(event) {
+        $('.head__burger,.head__row').toggleClass('active');
+    });
 });
