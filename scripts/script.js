f3ea92a4fffb77ee6353d1c1d628cb7f5dc276ea
dannydelimero <dendelime@gmail.com>
2022-08-14 22:27:50 +0300

    q

--- scripts/script.js
+++ scripts/script.js
@@ -12,6 +12,7 @@ $(document).ready(function() {
         infinite:true,
     });
     $('.head__burger').click(function(event) {
-        $('.head__burger,.head__row, body').toggleClass('active');
+        $('.head__burger,.head__row').toggleClass('active');
+        $('body').toggleClass('lock');
     });
 });
