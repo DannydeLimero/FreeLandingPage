a2b9b2e03c57cceeda16f903e9fd977f4c50d986
dannydelimero <dendelime@gmail.com>
2022-08-15 22:05:24 +0300

    q

--- scripts/script.js
+++ scripts/script.js
@@ -13,30 +13,23 @@ $(document).ready(function() {
     });
     $('.head__burger').click(function(event) {
         $('.head__burger,.head__row,.head__language').toggleClass('active');
-        $('body').addClass('lock');
+        $('body').toggleClass('lock');
     });
     $('.head__ru').click(function(event) {
         $('.head__eng').removeClass('active');
-    });
-    $('.head__ru').click(function(event) {
         $('.head__ru').addClass('active');
     });
     $('.head__eng').click(function(event) {
         $('.head__ru').removeClass('active');
-    });
-    $('.head__eng').click(function(event) {
         $('.head__eng').addClass('active');
     });
+   
     $('.main__ru').click(function(event) {
         $('.main__eng').removeClass('active');
-    });
-    $('.main__ru').click(function(event) {
         $('.main__ru').addClass('active');
     });
     $('.main__eng').click(function(event) {
         $('.main__ru').removeClass('active');
-    });
-    $('.main__eng').click(function(event) {
         $('.main__eng').addClass('active');
     });
 });
